8da42167953e50bd61c230d3b24e3edc3f6701ff
Mustafa Ali Dikçinar <mustafaalidikcinar@hotmail.com>
2022-11-02 00:54:25 +0300

    refactor

--- bin/file_encryption.dart
+++ bin/file_encryption.dart
@@ -1,16 +1,16 @@
 import 'dart:io';
-
 import 'package:encrypt/encrypt.dart';
 import 'package:file_encryption/file_encryption.dart' as file_encryption;
 
-final inputFolderPath = 'input';
-final outputFolderPath = 'output';
-final securedKey = 'yA7exe4ZFq5g2St1VDDexMJ5yr+8w+0RWivkfIrUVmc=';
+const inputFolderPath = 'input';
+const outputFolderPath = 'output';
+const secureKey = 'yA7exe4ZFq5g2St1VDDexMJ5yr+8w+0RWivkfIrUVmc=';
 
 void main(List<String> arguments) async {
-  final key = Key.fromBase64(securedKey);
+  final key = Key.fromBase64(secureKey);
   final iv = IV.fromLength(16);
   final encrypter = Encrypter(AES(key));
+  //Read files inside input folder and encrypt each of it
   final directory = Directory(inputFolderPath);
   List<FileSystemEntity> fileSystemEntities = directory.listSync();
   for (final fileSystemEntity in fileSystemEntities) {
@@ -20,6 +20,6 @@ void main(List<String> arguments) async {
       iv: iv,
       file: file,
       outputFolderPath: outputFolderPath,
-    ); 
+    );
   }
 }
